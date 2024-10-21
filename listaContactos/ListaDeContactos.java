b19be6dae485193de7c3845e8787513f63c1012b
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-10-21 14:53:03 +0000

    Actualizacion lista de contactos

--- listaContactos/ListaDeContactos.java
+++ listaContactos/ListaDeContactos.java
@@ -41,6 +41,55 @@ public class ListaDeContactos {
 
     }
 
+    
+     // Elimina un comtacto dando sus nombres y su apellidos
+     //
+     public boolean eliminarContacto(String nombres, String apellidos) throws PosicionIlegalException{
+        Contacto con = buscarContacto(nombres,apellidos);
+        if (con!=null){
+            for(int i=0;i<this.contactos.getTamanio();i++){
+                Contacto contAux=contactos.getValor(i);
+                if(contAux.getNombres().equals(nombres) && contAux.getApellido().equals(apellidos) ){
+                    contactos.remover(i);
+                }
+            }
+            return true;
+
+        }else 
+            return false;
+       
+
+     }
+
+    /**
+     * Modifica los datos de un contacto dado sus nombres y apellidos
+     * @param nombres
+     * @param apellidos
+     * @param direccion
+     * @param correo
+     * @param telefono
+     * @param celular
+     * @return true si se pudo modificar y false si mo se pudo
+     * @throws PosicionIlegalException
+     */
+
+    public boolean modificarContacto(String nombres, String apellidos, String direccion,
+            String correo, String telefono, String celular) throws PosicionIlegalException{
+        //verificar que el contacto exista
+        Contacto con=buscarContacto(nombres, apellidos);
+        if(con==null){
+            return false;
+        }
+        //Modifica el contacto con los datos nuevos
+        con.setDireccion(direccion);
+        con.setCorreo(correo);
+        con.setTelefono(telefono);
+        con.setCelular(celular);
+        return true;
+
+    }
+
+
     public Contacto buscarContacto(String nombres, String apellidos) 
         throws PosicionIlegalException{
             for (int i=0;i<contactos.getTamanio();i++){
